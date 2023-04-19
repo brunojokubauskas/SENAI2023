09889291002799777cc2c7e89559a977ccc0a549
brunojokubauskas <brunojokubauskas@gmail.com>
2023-04-19 11:13:42 -0300

    Provavalmente, o commit final.

--- prova/back/src/routes/routes.js
+++ prova/back/src/routes/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express.Router();
 //Vai receber os dados em json...
-app.use(express.json());
+//app.use(express.json());
+
+const Loja = require("../controllers/produto.controller");
 
 app.get("/vendedores", (req, res) => {
   // Endpoint para retornar nome do vendedor, matricula, total vendido e comissão (5%)
